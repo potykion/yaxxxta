738b0c1c685207ca9810a4d1d9dfd1dfc5ee4987
potykion <potykion@gmail.com>
2021-03-10 15:40:33 +0300

    One more test

--- test/habit/domain/services_test.dart
+++ test/habit/domain/services_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
@@ -12,35 +13,78 @@ import 'services_test.mocks.dart';
   [NotificationSender],
 )
 void main() {
-  test("ScheduleNotificationsForHabitsWithoutNotifications", () async {
-    var now = DateTime(2020, 1, 1, 11);
-    var habit = Habit.blank(
-      created: now,
-      performTime: DateTime(2020, 1, 1, 12),
-      isNotificationsEnabled: true,
-    );
+  group("ScheduleNotificationsForHabitsWithoutNotifications", () {
+    test("обычный кейс", () async {
+      var now = DateTime(2020, 1, 1, 11);
+      var habit = Habit.blank(
+        created: now,
+        performTime: DateTime(2020, 1, 1, 12),
+        isNotificationsEnabled: true,
+      );
 
-    var sender = MockNotificationSender();
-    when(sender.getAllPending()).thenAnswer((_) async => []);
-    when(sender.schedule(
-      title: anyNamed("title"),
-      body: anyNamed("body"),
-      sendAfterSeconds: anyNamed("sendAfterSeconds"),
-      payload: anyNamed("payload"),
-    )).thenAnswer((_) async => 0);
+      var sender = MockNotificationSender();
+      when(sender.getAllPending()).thenAnswer((_) async => []);
+      when(sender.schedule(
+        title: anyNamed("title"),
+        body: anyNamed("body"),
+        sendAfterSeconds: anyNamed("sendAfterSeconds"),
+        payload: anyNamed("payload"),
+      )).thenAnswer((_) async => 0);
 
-    var schedule = ScheduleNotificationsForHabitsWithoutNotifications(
-      notificationSender: sender,
-    );
-    await schedule([habit], now: now);
+      var schedule = ScheduleNotificationsForHabitsWithoutNotifications(
+        notificationSender: sender,
+      );
+      await schedule([habit], now: now);
 
-    verify(
-      sender.schedule(
-        title: habit.title,
-        body: "Пора выполнить привычку",
-        sendAfterSeconds: habit.performTime!.difference(now).inSeconds,
-        payload: jsonEncode({"habitId": habit.id}),
-      ),
-    );
+      verify(
+        sender.schedule(
+          title: habit.title,
+          body: "Пора выполнить привычку",
+          sendAfterSeconds: habit.performTime!.difference(now).inSeconds,
+          payload: jsonEncode({"habitId": habit.id}),
+        ),
+      );
+    });
+
+    test("кейс, когда есть заскедуленные привычки", () async {
+      var now = DateTime(2020, 1, 1, 11);
+      var habit = Habit.blank(
+        created: now,
+        performTime: DateTime(2020, 1, 1, 12),
+        isNotificationsEnabled: true,
+      ).copyWith(
+        id: "1",
+      );
+
+      var sender = MockNotificationSender();
+      when(sender.getAllPending()).thenAnswer((_) async => [
+            PendingNotificationRequest(
+              1,
+              "sam",
+              'sam',
+              jsonEncode({"habitId": "2"}),
+            ),
+          ]);
+      when(sender.schedule(
+        title: anyNamed("title"),
+        body: anyNamed("body"),
+        sendAfterSeconds: anyNamed("sendAfterSeconds"),
+        payload: anyNamed("payload"),
+      )).thenAnswer((_) async => 0);
+
+      var schedule = ScheduleNotificationsForHabitsWithoutNotifications(
+        notificationSender: sender,
+      );
+      await schedule([habit], now: now);
+
+      verify(
+        sender.schedule(
+          title: habit.title,
+          body: "Пора выполнить привычку",
+          sendAfterSeconds: habit.performTime!.difference(now).inSeconds,
+          payload: jsonEncode({"habitId": habit.id}),
+        ),
+      );
+    });
   });
 }
