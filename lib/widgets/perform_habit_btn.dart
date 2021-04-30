ce38843afc88e7ac2056c033e8cd4b11963daa98
potykion <potykion@gmail.com>
2021-04-30 16:30:57 +0300

    Show animation on habit perform

--- lib/widgets/perform_habit_btn.dart
+++ lib/widgets/perform_habit_btn.dart
@@ -1,24 +1,44 @@
+import 'dart:math';
+
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:yaxxxta/logic/habit/controllers.dart';
 import 'package:yaxxxta/logic/habit/vms.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../theme.dart';
 
-class PerformHabitButton extends StatelessWidget {
+class PerformHabitButton extends HookWidget {
   final HabitVM vm;
   final double size;
   final void Function()? onPerform;
 
-  const PerformHabitButton(
-      {Key? key, required this.vm, this.size = 90, this.onPerform})
-      : super(key: key);
+  PerformHabitButton({
+    Key? key,
+    required this.vm,
+    this.size = 90,
+    this.onPerform,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var confettiController = useMemoized(
+      () => ConfettiController(duration: Duration(milliseconds: 500)),
+    );
+    useEffect(() => confettiController.dispose, []);
+
     return Stack(
       alignment: Alignment.center,
       children: [
+        ConfettiWidget(
+          confettiController: confettiController,
+          blastDirectionality: BlastDirectionality.explosive,
+          colors: [
+            CustomColors.yellow
+          ],
+          createParticlePath: drawStar,
+        ),
         SizedBox(
           width: size + 10,
           height: size + 10,
@@ -35,12 +55,41 @@ class PerformHabitButton extends StatelessWidget {
             heroTag: null,
             child: Icon(Icons.done, size: size / 2),
             onPressed: () async {
-              await context.read(habitControllerProvider.notifier).perform(vm.habit);
+              await context
+                  .read(habitControllerProvider.notifier)
+                  .perform(vm.habit);
+              confettiController.play();
+              await Future<void>.delayed(confettiController.duration);
               if (onPerform != null) onPerform!();
             },
           ),
         ),
       ],
     );
+  }
+
+  /// A custom Path to paint stars.
+  Path drawStar(Size size) {
+    // Method to convert degree to radians
+    double degToRad(double deg) => deg * (pi / 180.0);
+
+    const numberOfPoints = 5;
+    final halfWidth = size.width / 2;
+    final externalRadius = halfWidth;
+    final internalRadius = halfWidth / 2.5;
+    final degreesPerStep = degToRad(360 / numberOfPoints);
+    final halfDegreesPerStep = degreesPerStep / 2;
+    final path = Path();
+    final fullAngle = degToRad(360);
+    path.moveTo(size.width, halfWidth);
+
+    for (double step = 0; step < fullAngle; step += degreesPerStep) {
+      path.lineTo(halfWidth + externalRadius * cos(step),
+          halfWidth + externalRadius * sin(step));
+      path.lineTo(halfWidth + internalRadius * cos(step + halfDegreesPerStep),
+          halfWidth + internalRadius * sin(step + halfDegreesPerStep));
+    }
+    path.close();
+    return path;
   }
 }
