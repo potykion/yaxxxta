36b5dacf1b072c19e2bd5d005c412c004719d89c
potykion <potykion@gmail.com>
2021-05-18 02:10:18 +0300

    So so

--- lib/ui/calendar/app/habit_swiper.dart
+++ lib/ui/calendar/app/habit_swiper.dart
@@ -2,87 +2,109 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:yaxxxta/logic/app_user_info/controllers.dart';
 import 'package:yaxxxta/logic/habit/controllers.dart';
 import 'package:yaxxxta/logic/habit/vms.dart';
 
 enum Swipe { rightToLeft, leftToRight }
 
 Swipe createSwipe(int indexFrom, int indexTo, int length) {
+  if (indexTo == length - 1 && indexFrom == 0) return Swipe.leftToRight;
   if (indexFrom > indexTo) return Swipe.leftToRight;
   return Swipe.rightToLeft;
 }
 
 typedef SwipeTo = Future<void> Function(int index, {bool toUnperformed});
-typedef HabitSwiperItemBuilder = Widget Function(
-    BuildContext context, int index, SwipeTo swipeTo);
 
-
-class HabitSwiper extends HookWidget {
-  final HabitSwiperItemBuilder builder;
+class HabitSwiperController {
   final List<HabitVM> habits;
-  final PageController controller;
+  final bool swipeToNextUnperformed;
+
+  late PageController pageController;
+  int currentIndex = 0;
+  bool swipeExact = false;
 
   int get habitCount => habits.length;
 
+  HabitSwiperController({
+    required this.habits,
+    required this.swipeToNextUnperformed,
+  }) {
+    if (swipeToNextUnperformed) {
+      currentIndex = max(
+        getNextUnperformedHabitIndex(habits, includeInitial: true),
+        0,
+      );
+    }
+    pageController = PageController(
+      initialPage: 1000 * habitCount + currentIndex,
+    );
+  }
+
+  /// Функция, которая свайпает на страницу [normalizedIndexToSwipe]
+  ///
+  /// Если [toUnperformed] = true, то свайп осуществляется на очередную
+  /// невыполненную привычку (в зависимости от направления свайпа)
+  /// Если [swipeToNextUnperformed] = false,
+  /// то будет свайп на очередную привычку
+  void swipeTo(int indexToSwipe, {bool toUnperformed = false}) {
+    var normalizedIndexToSwipe = normalizeIndex(indexToSwipe);
+
+    if (!toUnperformed) {
+      currentIndex = normalizedIndexToSwipe;
+      swipeExact = true;
+      pageController.jumpToPage(loopIndex(currentIndex));
+      return;
+    }
+
+    if (!swipeToNextUnperformed) return;
+
+    if (swipeExact) {
+      swipeExact = false;
+      return;
+    }
+
+    var swipe = createSwipe(currentIndex, normalizedIndexToSwipe, habitCount);
+    var indexGetter = swipe == Swipe.rightToLeft
+        ? getNextUnperformedHabitIndex
+        : getPreviousUnperformedHabitIndex;
+    var nextIndex = indexGetter(
+      habits,
+      initialIndex: normalizedIndexToSwipe,
+      includeInitial: true,
+    );
+
+    currentIndex = normalizedIndexToSwipe;
+
+    if (nextIndex == -1 || nextIndex == normalizedIndexToSwipe) return;
+    currentIndex = nextIndex;
+
+    pageController.animateToPage(
+      loopIndex(currentIndex),
+      curve: Curves.easeIn,
+      duration: Duration(seconds: 1),
+    );
+  }
+
+  int normalizeIndex(int index) => index % habitCount;
+
+  int loopIndex(int index) => 1000 * habitCount + index;
+}
+
+class HabitSwiper extends HookWidget {
+  IndexedWidgetBuilder builder;
+  HabitSwiperController controller;
+
   HabitSwiper({
     required this.builder,
-    required this.habits,
     required this.controller,
   });
 
   @override
-  Widget build(BuildContext context) {
-    var swipeToNextUnperformedEnabled =
-        useProvider(swipeToNextUnperformedProvider);
-
-    var initialPage = useMemoized(
-      () => swipeToNextUnperformedEnabled
-          ? max(getNextUnperformedHabitIndex(habits, includeInitial: true), 0)
-          : 0,
-    );
-
-    /// Функция, которая свайпает на страницу [indexToSwipe]
-    ///
-    /// Если [toUnperformed] = true, то свайп осуществляется на очередную
-    /// невыполненную привычку (в зависимости от направления свайпа)
-    /// Если [swipeToNextUnperformed] = false,
-    /// то будет свайп на очередную привычку
-    swipeTo(int indexToSwipe, {bool toUnperformed = false}) async {
-      if (!toUnperformed) {
-        controller.jumpToPage(1000 * habitCount + indexToSwipe);
-        return;
-      }
-
-      if (!swipeToNextUnperformedEnabled) return;
-
-      var swipe = createSwipe(initialPage, indexToSwipe, habitCount);
-      var indexGetter = swipe == Swipe.rightToLeft
-          ? getNextUnperformedHabitIndex
-          : getPreviousUnperformedHabitIndex;
-
-      var nextIndex = indexGetter(
-        habits,
-        initialIndex: indexToSwipe,
-        includeInitial: true,
+  Widget build(BuildContext context) => PageView.builder(
+        controller: controller.pageController,
+        itemBuilder: (context, index) =>
+            builder(context, controller.normalizeIndex(index)),
+        onPageChanged: (index) =>
+            controller.swipeTo(index, toUnperformed: true),
       );
-      if (nextIndex == -1 || nextIndex == indexToSwipe) return;
-
-      controller.jumpToPage(1000 * habitCount + nextIndex);
-    }
-
-    return PageView.builder(
-      controller: controller,
-      itemBuilder: (context, index) => builder(
-        context,
-        index % habitCount,
-        swipeTo,
-      ),
-      onPageChanged: (index) => swipeTo(
-        index % habitCount,
-        toUnperformed: true,
-      ),
-    );
-  }
 }
--- lib/ui/calendar/app/page.dart
+++ lib/ui/calendar/app/page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:yaxxxta/logic/app_user_info/controllers.dart';
 import 'package:yaxxxta/logic/habit/controllers.dart';
 import 'package:yaxxxta/logic/habit/vms.dart';
 import 'package:yaxxxta/ui/calendar/app/calendar_appbar.dart';
@@ -43,28 +44,32 @@ class CalendarAppPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     List<HabitVM> vms = useProvider(habitVMsProvider);
+    var swipeToNextUnperformed = useProvider(swipeToNextUnperformedProvider);
 
     var controller = useMemoized(
-      () => PageController(initialPage: 1000 * vms.length + 0),
+      () => HabitSwiperController(
+        habits: vms,
+        swipeToNextUnperformed: swipeToNextUnperformed,
+      ),
     );
 
     return Scaffold(
-      appBar: CalendarAppBar(
-        onHabitSelect: (index) =>
-            controller.jumpToPage(1000 * vms.length + index),
-      ),
+      appBar: CalendarAppBar(onHabitSelect: controller.swipeTo),
       body: vms.isEmpty
           ? Center(child: Text("Привычки не найдены"))
           : HabitSwiper(
-              habits: vms,
               controller: controller,
-              builder: (context, index, swipeTo) => Column(
+              builder: (context, index) {
+                print(index);
+                return Column(
                 children: [
                   Expanded(
                     child: HabitPerformingCard(
                       vm: vms[index],
-                      onPerform: () => swipeTo(index, toUnperformed: true),
-                      onArchive: () => swipeTo(0),
+                      onPerform: () =>
+                          controller.swipeTo(index, toUnperformed: true),
+                      onArchive: () =>
+                          controller.swipeTo(0, toUnperformed: true),
                     ),
                   ),
                   Padding(
@@ -80,7 +85,8 @@ class CalendarAppPage extends HookWidget {
                     },
                   ),
                 ],
-              ),
+              );
+              },
             ),
     );
   }
