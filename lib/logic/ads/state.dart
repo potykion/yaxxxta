184c5d622a16c8c2ef5ab77ac05bb6b83ce8156c
potykion <potykion@gmail.com>
2021-06-23 19:46:19 +0300

    Page view instead of swiper

--- lib/logic/ads/state.dart
+++ lib/logic/ads/state.dart
@@ -4,12 +4,9 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:yaxxxta/logic/app_user_info/controllers.dart';
 
-FutureProvider<bool> _isPhysicalDeviceProvider = FutureProvider(
-      (_) async => (await DeviceInfoPlugin().androidInfo).isPhysicalDevice ?? false,
-);
 
 FutureProvider<InitializationStatus?> _adsInitializedProvider = FutureProvider(
-      (ref) async {
+  (ref) async {
     if (kIsWeb) return null;
     if (ref.watch(appUserInfoControllerProvider).haveSubscription) return null;
     return MobileAds.instance.initialize();
@@ -17,21 +14,17 @@ FutureProvider<InitializationStatus?> _adsInitializedProvider = FutureProvider(
 );
 
 var adProvider = Provider.family(
-      (ref, __) => ref.watch(_adsInitializedProvider).maybeWhen(
-    data: (status) => status != null
-        ? ref.watch(_isPhysicalDeviceProvider).maybeWhen(
-      data: (isPhysicalDevice) => BannerAd(
-        adUnitId: isPhysicalDevice
-            ? "ca-app-pub-6011780463667583/9890116434"
-        // ? BannerAd.testAdUnitId
-            : BannerAd.testAdUnitId,
-        size: AdSize.banner,
-        request: AdRequest(),
-        listener: AdListener(),
-      )..load(),
-      orElse: () => null,
-    )
-        : null,
-    orElse: () => null,
-  ),
+  (ref, __) => ref.watch(_adsInitializedProvider).maybeWhen(
+        data: (status) => status != null
+            ? (BannerAd(
+                adUnitId: kReleaseMode
+                    ? "ca-app-pub-6011780463667583/9890116434"
+                    : BannerAd.testAdUnitId,
+                size: AdSize.banner,
+                request: AdRequest(),
+                listener: AdListener(),
+              )..load())
+            : null,
+        orElse: () => null,
+      ),
 );
--- lib/ui/calendar/app/habit_swiper.dart
+++ lib/ui/calendar/app/habit_swiper.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:yaxxxta/logic/ads/state.dart';
@@ -14,7 +14,7 @@ import 'package:yaxxxta/widgets/habit_performing_calendar.dart';
 import 'habit_form.dart';
 import 'habit_stats.dart';
 
-class HabitSwiper extends StatelessWidget {
+class HabitSwiper extends HookWidget {
   final List<HabitVM> vms;
 
   const HabitSwiper({
@@ -23,74 +23,79 @@ class HabitSwiper extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Swiper(
-        // В релиз-моде делаем скейл карточек красивый
-        // В дебаг-моде он тормозит
-        // viewportFraction: kReleaseMode ? 0.9 : 1,
-        // scale: kReleaseMode ? 0.9 : 1,
-        viewportFraction: 0.9,
-        scale: 0.9,
-        itemBuilder: (context, index) {
-          var vm = vms[index];
+  Widget build(BuildContext context) {
+    var pageController = useMemoized(
+      () => PageController(
+        initialPage: vms.length * 10000,
+        viewportFraction: 0.95,
+      ),
+    );
+    useEffect(() => () => pageController.dispose, []);
 
-          return Consumer(
-            builder: (context, watch, child) {
-              var ad = watch(adProvider(DateTime.now().millisecondsSinceEpoch));
-              return CoreCard(
-                color: vm.isPerformedToday ? Color(0xfff1fafa) : null,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Headline5(
-                      vm.habit.title,
-                      trailing: IconButton(
-                        onPressed: () async {
-                          var habit = await showHabitFormBottomSheet(
-                            context,
-                            initial: vm.habit,
-                          );
-                          if (habit != null) {
-                            await context
-                                .read(habitControllerProvider.notifier)
-                                .update(habit);
-                          }
-                        },
-                        icon: Icon(Icons.edit),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Headline6("Прогресс"),
-                        HabitPerformingCalendar(vm: vm),
-                        CoreButton(
-                          text: "Выполнить",
-                          icon: Icons.done,
-                          onPressed: () => context
+    return PageView.builder(
+      controller: pageController,
+      itemBuilder: (context, index) {
+        var vm = vms[index % vms.length];
+
+        return Consumer(
+          builder: (context, watch, child) {
+            var ad = watch(adProvider(index));
+            return CoreCard(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+              color: vm.isPerformedToday ? Color(0xfff1fafa) : null,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Headline5(
+                    vm.habit.title,
+                    trailing: IconButton(
+                      onPressed: () async {
+                        var habit = await showHabitFormBottomSheet(
+                          context,
+                          initial: vm.habit,
+                        );
+                        if (habit != null) {
+                          await context
                               .read(habitControllerProvider.notifier)
-                              .perform(vm.habit),
-                        ),
-                      ],
+                              .update(habit);
+                        }
+                      },
+                      icon: Icon(Icons.edit),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Headline6("Статистика"),
-                        HabitStats(vm: vm),
-                      ],
-                    ),
-                    Container(
-                      height: AdSize.banner.height.toDouble(),
-                      width: AdSize.banner.width.toDouble(),
-                      child: ad != null ? AdWidget(ad: ad) : null,
-                    ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
-        itemCount: vms.length,
-      );
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Headline6("Прогресс"),
+                      HabitPerformingCalendar(vm: vm),
+                      CoreButton(
+                        text: "Выполнить",
+                        icon: Icons.done,
+                        onPressed: () => context
+                            .read(habitControllerProvider.notifier)
+                            .perform(vm.habit),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Headline6("Статистика"),
+                      HabitStats(vm: vm),
+                    ],
+                  ),
+                  Container(
+                    height: AdSize.banner.height.toDouble(),
+                    width: AdSize.banner.width.toDouble(),
+                    child: ad != null ? AdWidget(ad: ad) : null,
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
 }
